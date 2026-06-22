991e25cdb908b8505c36d4500b62fc3b49261124
James Garbutt <43081j@users.noreply.github.com>
2026-06-22 20:32:06 +0100

    feat: switch to ESM only
    Since we now require Node 20.x, we can safely switch to ESM only as all
    LTS of Node support `require(esm)`. This means CJS and ESM users can
    import or require the module either way.
    
    For browsers, it is just expected they are running a bundler or module
    resolver anyway.

--- test/fail.js
+++ test/fail.js
@@ -1,166 +1,149 @@
-(function () {
-  'use strict';
-  /*jshint -W003*/
-  if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
-    // NodeJS
-    getPayload(require('chai'), require('../index'));
-  } else if (typeof define === 'function' && define.amd) {
-    // AMD
-    define([
-      'chai',
-      'chai-json-schema',
-    ], function (chai, plugin) {
-      return getPayload(chai, plugin);
+import * as chai from 'chai';
+import plugin from '../index.js';
+
+chai.use(plugin);
+chai.should();
+chai.config.includeStack = true;
+
+const assert = chai.assert;
+
+describe('chai-json-schema fail demo', function () {
+  chai.use(function (chai, utils) {
+    const inspect = utils.objDisplay;
+    chai.Assertion.addMethod('fail', function (message) {
+      const obj = this._obj;
+
+      new chai.Assertion(obj).is.a('function');
+
+      try {
+        obj();
+      } catch (err) {
+        this.assert(
+          err instanceof chai.AssertionError,
+          message + ' expected #{this} to fail, but it threw ' + inspect(err)
+        );
+        return;
+      }
+
+      this.assert(false, message + ' expected #{this} to fail');
     });
-  } else {
-    // Other environment (usually <script> tag): plug in to global chai instance directly.
-    return getPayload(window.chai, null);
-  }
-
-  function getPayload(chai, plugin) {
-    if (plugin) {
-      chai.use(plugin);
-    }
-    chai.should();
-    chai.config.includeStack = true;
-
-    var assert = chai.assert;
-
-    describe('chai-json-schema fail demo', function () {
-      chai.use(function (chai, utils) {
-        var inspect = utils.objDisplay;
-        chai.Assertion.addMethod('fail', function (message) {
-          var obj = this._obj;
-
-          new chai.Assertion(obj).is.a('function');
-
-          try {
-            obj();
-          } catch (err) {
-            this.assert(
-              err instanceof chai.AssertionError
-              , message + ' expected #{this} to fail, but it threw ' + inspect(err));
-            return;
+  });
+  const tests = [
+    {
+      name: 'properties',
+      schema: {
+        properties: {
+          intKey: {
+            type: 'integer'
+          },
+          stringKey: {
+            type: 'string'
           }
-
-          this.assert(false, message + ' expected #{this} to fail');
-        });
-      });
-      var tests = [{
-        name: 'properties',
-        schema: {
-          properties: {
-            intKey: {
-              type: 'integer'
-            },
-            stringKey: {
-              type: 'string'
-            }
-          }
-        },
-        valid: [{
+        }
+      },
+      valid: [
+        {
           data: {
             intKey: 1,
             stringKey: 'one'
           }
-        }],
-        invalid: [{
+        }
+      ],
+      invalid: [
+        {
           data: {
             intKey: 3,
             stringKey: false
           }
-        }]
-      }, {
-        name: 'combinations',
-        schema: {
-          id: 'any_v1',
-          anyOf: [
-            { type: 'integer' },
-            { type: 'string' }
-          ]
-        },
-        valid: [
-          { data: 1 },
-          { data: 'yo' }
-        ],
-        invalid: [
-          { data: [1, 2, 3] },
-          { data: { aa: 1 } }
-        ]
-      }, {
-        name: 'fruit',
-        schema: {
-          id: 'fruit_v1',
-          description: 'fresh fruit schema v1',
-          type: 'object',
-          properties: {
-            required: ['skin', 'colors', 'taste'],
-            colors: {
-              type: 'array',
-              minItems: 1,
-              uniqueItems: true,
-              items: {
-                type: 'string'
-              }
-            },
-            skin: {
+        }
+      ]
+    },
+    {
+      name: 'combinations',
+      schema: {
+        id: 'any_v1',
+        anyOf: [{ type: 'integer' }, { type: 'string' }]
+      },
+      valid: [{ data: 1 }, { data: 'yo' }],
+      invalid: [{ data: [1, 2, 3] }, { data: { aa: 1 } }]
+    },
+    {
+      name: 'fruit',
+      schema: {
+        id: 'fruit_v1',
+        description: 'fresh fruit schema v1',
+        type: 'object',
+        properties: {
+          required: ['skin', 'colors', 'taste'],
+          colors: {
+            type: 'array',
+            minItems: 1,
+            uniqueItems: true,
+            items: {
               type: 'string'
-            },
-            taste: {
-              type: 'number',
-              minimum: 5
             }
+          },
+          skin: {
+            type: 'string'
+          },
+          taste: {
+            type: 'number',
+            minimum: 5
           }
-        },
-        valid: [{
+        }
+      },
+      valid: [
+        {
           data: {
             skin: 'thin',
             colors: ['red', 'green', 'yellow'],
             taste: 10
           }
-        }],
-        invalid: [{
+        }
+      ],
+      invalid: [
+        {
           data: {
             colors: ['brown'],
             taste: 0,
             worms: 2
           }
-        }]
-      }];
+        }
+      ]
+    }
+  ];
 
-      it('has tests', function () {
-        assert.isArray(tests, 'tests');
-        assert.operator(tests.length, '>', 0, 'tests.length');
+  it('has tests', function () {
+    assert.isArray(tests, 'tests');
+    assert.operator(tests.length, '>', 0, 'tests.length');
+  });
+
+  Object.keys(tests).forEach(function (key) {
+    const testCase = tests[key];
+    describe('test ' + testCase.name, function () {
+      it('has settings', function () {
+        assert.isObject(testCase.schema, 'schema');
+
+        assert.isArray(testCase.valid, 'valid');
+        assert.operator(testCase.valid.length, '>', 0, 'valid.length');
+
+        assert.isArray(testCase.invalid, 'invalid');
+        assert.operator(testCase.invalid.length, '>', 0, 'invalid.length');
       });
+    });
+  });
 
-      Object.keys(tests).forEach(function (key) {
-        var testCase = tests[key];
-        describe('test ' + testCase.name, function () {
-          it('has settings', function () {
-            assert.isObject(testCase.schema, 'schema');
-
-            assert.isArray(testCase.valid, 'valid');
-            assert.operator(testCase.valid.length, '>', 0, 'valid.length');
-
-            assert.isArray(testCase.invalid, 'invalid');
-            assert.operator(testCase.invalid.length, '>', 0, 'invalid.length');
-          });
-        });
-      });
-
-      // enable this to see the error reporting
-      describe('bulk', function () {
-        Object.keys(tests).forEach(function (key) {
-          var testCase = tests[key];
-          describe(testCase.name + ' schema', function () {
-            it('assert.jsonSchema()', function () {
-              testCase.invalid.forEach(function (obj, i) {
-                assert.notJsonSchema(obj.data, testCase.schema, '#' + i);
-              });
-            });
+  // enable this to see the error reporting
+  describe('bulk', function () {
+    Object.keys(tests).forEach(function (key) {
+      const testCase = tests[key];
+      describe(testCase.name + ' schema', function () {
+        it('assert.jsonSchema()', function () {
+          testCase.invalid.forEach(function (obj, i) {
+            assert.notJsonSchema(obj.data, testCase.schema, '#' + i);
           });
         });
       });
     });
-  }
-}());
+  });
+});
